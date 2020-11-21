e7d1be741b8b0400fd993246d72b682386f215de
Gold Harvest <googincheng@gmail.com>
2020-11-21 16:12:28 +0800

    title modified

--- extension/devtools/devtools_init_page.js
+++ extension/devtools/devtools_init_page.js
@@ -1,1 +1,1 @@
-chrome.devtools.panels.create("Web Scraper", "../assets/images/icon48.png", "devtools/devtools_scraper_panel.html");
+chrome.devtools.panels.create("Goo Scraper", "../assets/images/icon48.png", "devtools/devtools_scraper_panel.html");
